7e7a1a8750095dcef3a4178434cfbb0c1d30e33c
Azerty <nik.k.2018@mail.ru>
2025-06-08 20:26:03 +0300

    Задача 1 задача 2 задача 3

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+public class Task4 {
+    public static void main(String[] args) {
+        var friend = 19;
+
+        System.out.println("Исходное значение: " + friend);
+
+        friend += 2;
+        System.out.println("После увеличения на 2: " + friend);
+
+        friend /= 7;
+        System.out.println("После деления на 7: " + friend);
+    }
+}
