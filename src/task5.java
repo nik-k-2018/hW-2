8271e09c3b9a8cc8a294f574a75013c3ae229d47
Azerty <nik.k.2018@mail.ru>
2025-06-08 20:39:28 +0300

    задача 5

---
+++ src/task5.java
@@ -0,0 +1,16 @@
+public class task5 {
+    public static void main(String[] args) {
+        var frog = 3.5;
+
+        System.out.println("Исходное значение: " + frog);
+
+        frog *= 10; // Увеличиваем в 10 раз
+        System.out.println("После умножения на 10: " + frog);
+
+        frog /= 3.5; // Делим на 3.5
+        System.out.println("После деления на 3.5: " + frog);
+
+        frog += 4; // Добавляем 4
+        System.out.println("После добавления 4: " + frog);
+    }
+}
